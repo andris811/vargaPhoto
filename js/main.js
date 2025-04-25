a536e7d3b09e7be49570cc310d62d170b56fbc2d
Andras Varga <vargan89@gmail.com>
2025-04-25 20:12:42 +0800

    modernized back-to-top button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// import { setupMobileMenu } from './navbar.js';
-// import { initLanguage } from './language.js';
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   waitForNavbar();
-// });
-
-// function waitForNavbar() {
-//   if (document.getElementById('nav-links')) {
-//     setupMobileMenu();
-//     initLanguage();
-//   } else {
-//     setTimeout(waitForNavbar, 100);
-//   }
-// }
-
-
 // Footer
 async function loadFooter() {
     try {
